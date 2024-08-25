8efc0a7cd44fdcb27585671615d3c0473ffeb9d5
mmi-ku <161840539+mmi-ku@users.noreply.github.com>
2024-08-25 05:16:48 +0000

    devise機能追加途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,34 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_path
+    when Customer
+      root_path
+    else
+      root_path
+    end
+  end
+  
+  def after_sign_up_path_for(resource)
+    case resource
+    when Admin
+      admin_path
+    when Customer
+      root_path
+    else
+      root_path
+    end
+  end
+  
+  def after_sign_out_path_for(resource)
+    case resource
+    when Admin
+      admin_path
+    when Customer
+      root_path
+    else
+      root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+devise_for :admin, kip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+get 'homes/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
